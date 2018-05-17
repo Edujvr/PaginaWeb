aef96729bdbe4229bdf7b3bc51bbf7beb1c21deb
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-05-17 11:37:43 -0500

    Update chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,4 +1,4 @@
-var obj = JSON.parse($("#dom-target").text());
+var atr = JSON.parse($("#dom-target").text());
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -6,8 +6,8 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-109367761-1', 'auto');
 ga('send', 'event', {
    'eventCategory': 'Intento',
-   'eventAction': obj.result.metadata.intentName,
-   'eventLabel': obj.result.fulfillment.speech
+   'eventAction': atr.result.metadata.intentName,
+   'eventLabel': atr.result.fulfillment.speech
                   }); 
 
 $(function () {
