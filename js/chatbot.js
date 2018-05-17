340fe2e1611aa3f0ce4fe4c71ed774f8ee309399
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-05-17 11:05:39 -0500

    Update chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,3 +1,10 @@
+$(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-109367761-1', 'auto');
+ga('send', 'pageview');
+
 $(function () {
 
     window.initialMessageDisplayed = false;
@@ -9,6 +16,13 @@ $(function () {
             var event = obj.result.action;
             var textFromDefaultResponse = obj.result.fulfillment.speech;
             var messages = obj.result.fulfillment.messages;
+            ga('send', 'event', {
+                'eventCategory': 'Intento',
+                'eventAction': obj.result.metadata.intentName,
+                'eventLabel': obj.result.fulfillment.speech'
+              });
+            
+            
             var answerRow = jQuery('<div/>',{
                     'class':'row'
                 });
