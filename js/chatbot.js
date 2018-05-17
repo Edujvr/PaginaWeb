aac686c0f7700d513ee720ce65896d37409e3ca6
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-05-17 11:22:52 -0500

    Update chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,14 +1,3 @@
-$(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-var obj = JSON.parse($("#dom-target").text());
-ga('create', 'UA-109367761-1', 'auto');
-ga('send', 'pageview');
-ga('send', 'event', {
-   'eventCategory': 'Intento',
-   'eventAction': obj.result.metadata.intentName,
-   'eventLabel': obj.result.fulfillment.speech'});
 $(function () {
 
     window.initialMessageDisplayed = false;
