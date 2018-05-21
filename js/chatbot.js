8c20b6805e6cda85dbbd0110adcc8a060a38bd61
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-05-21 09:44:40 -0500

    Update chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -4,6 +4,8 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-109367761-1', 'auto');
 
+var ip = java.net.InetAddress.getLocalHost().getHostAddress ();
+
 $(function () {
 
     window.initialMessageDisplayed = false;
